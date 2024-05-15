0981fda7bef7b6a98ed989ba2c7c18c4111d4f41
Daianabifiger <167824269+Daianabifiger@users.noreply.github.com>
2024-05-15 18:10:41 -0300

    Update Usuario.cs
    no es contraseña es contrasenia

--- PrimeraEntrega/Usuario.cs
+++ PrimeraEntrega/Usuario.cs
@@ -12,17 +12,17 @@ namespace PrimeraEntrega
         protected string Nombre { get; set; }
         protected string Apellido { get; set; }
         protected string NombreUsuario { get; set; }
-        protected string Contraseña { get; set; }
+        protected string Contrasenia { get; set; }
         protected string Mail { get; set; }
 
 
-        public Usuario(int id, string nombre, string apellido, string nombreUsuario, string contraseña, string mail) 
+        public Usuario(int id, string nombre, string apellido, string nombreUsuario, string contrasenia, string mail) 
         { 
             this.Id = id;
             this.Nombre = nombre;
             this.Apellido = apellido;
             this.NombreUsuario = nombreUsuario;
-            this.Contraseña = contraseña;
+            this.Contrasenia = contrasenia;
             this.Mail = mail;
         }
     }
